df79e4f079ca0f9f087e0100e05f563baa1431ce
danielWebDev-spec <danielwebdeveloper220@gmail.com>
2022-02-10 14:02:53 -0500

    slider

--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
 export { default as Navbar } from "./Navbar.jsx";
 export { default as Layout } from "./Layout.jsx";
 export { default as Footer } from "./Footer.jsx";
+export { default as Featured } from "./Featured.jsx";
