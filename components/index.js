cf2c20c8b4623814f200db5876db4790abef13d4
danielWebDev-spec <danielwebdeveloper220@gmail.com>
2022-02-10 14:30:13 -0500

    listing products

--- components/index.js
+++ components/index.js
@@ -2,3 +2,5 @@ export { default as Navbar } from "./Navbar.jsx";
 export { default as Layout } from "./Layout.jsx";
 export { default as Footer } from "./Footer.jsx";
 export { default as Featured } from "./Featured.jsx";
+export { default as ProductList } from "./ProductList.jsx";
+export { default as ProductCard } from "./ProductCard.jsx";
