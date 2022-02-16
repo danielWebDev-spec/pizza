9e11f5f1304cf3a7fa15bea71e34e63cfde9b484
Daniel <danielwebdeveloper220@gmail.com>
2022-02-15 21:12:49 -0500

    nextjs REST api

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -5,13 +5,34 @@ import axios from "axios";
 
 const Product = ({ pizza }) => {
   const [size, setSize] = useState(0);
-  // const pizza = {
-  //   id: 1,
-  //   img: "/img/pizza.png",
-  //   name: "CAMPAGNOLA",
-  //   price: [19.9, 23.9, 27.9],
-  //   desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis arcu purus, rhoncus fringilla vestibulum vel, dignissim vel ante. Nulla facilisi. Nullam a urna sit amet tellus pellentesque egestas in in ante.",
-  // };
+  const [price, setPrice] = useState(pizza.prices[0]);
+  const [quantity, setQuantity] = useState(1);
+  const [extras, setExtras] = useState([]);
+
+  const changePrice = (number) => {
+    setPrice(price + number);
+  };
+
+  const handleChange = (e, option) => {
+    const checked = e.target.checked;
+
+    if (checked) {
+      changePrice(option.price);
+      // setExtras((prev) => [...prev, option]);
+      setExtras([...extras, option]);
+    } else {
+      changePrice(-option.price);
+      setExtras(extras.filter((extra) => extra._id !== option._id));
+    }
+  };
+
+  // console.log(extras);
+
+  const handleSize = (sizeIndex) => {
+    const difference = pizza.prices[sizeIndex] - pizza.prices[size];
+    setSize(sizeIndex);
+    changePrice(difference);
+  };
 
   return (
     <div className={styles.container}>
@@ -22,19 +43,19 @@ const Product = ({ pizza }) => {
       </div>
       <div className={styles.right}>
         <h1 className={styles.title}>{pizza.title}</h1>
-        <span className={styles.price}>${pizza.prices[size]}</span>
+        <span className={styles.price}>${price}</span>
         <p className={styles.desc}>{pizza.desc}</p>
         <h3 className={styles.choose}>Choose the size</h3>
         <div className={styles.sizes}>
-          <div className={styles.size} onClick={() => setSize(0)}>
+          <div className={styles.size} onClick={() => handleSize(0)}>
             <Image src="/img/size.png" layout="fill" alt="" />
             <span className={styles.number}>Small</span>
           </div>
-          <div className={styles.size} onClick={() => setSize(1)}>
+          <div className={styles.size} onClick={() => handleSize(1)}>
             <Image src="/img/size.png" layout="fill" alt="" />
             <span className={styles.number}>Medium</span>
           </div>
-          <div className={styles.size} onClick={() => setSize(2)}>
+          <div className={styles.size} onClick={() => handleSize(2)}>
             <Image src="/img/size.png" layout="fill" alt="" />
             <span className={styles.number}>Large</span>
           </div>
@@ -45,16 +66,22 @@ const Product = ({ pizza }) => {
             <div className={styles.option} key={option._id}>
               <input
                 type="checkbox"
-                id="double"
-                name="double"
+                id={option.text}
+                name={option.text}
                 className={styles.checkbox}
+                onChange={(e) => handleChange(e, option)}
               />
               <label htmlFor="double">{option.text}</label>
             </div>
           ))}
         </div>
         <div className={styles.add}>
-          <input type="number" defaultValue={1} className={styles.quantity} />
+          <input
+            onChange={(e) => setQuantity(e.target.value)}
+            type="number"
+            defaultValue={1}
+            className={styles.quantity}
+          />
           <button className={styles.button}>Add to Cart</button>
         </div>
       </div>
